02fb0e0870bcfe87386b9f33543003e532dc2b3d
jorge-rufino <jorge.rufino1986@gmail.com>
2023-12-08 11:42:11 -0300

    Criando projeto

---
+++ src/main/java/com/rufino/climedapi/ClimedApiApplication.java
@@ -0,0 +1,13 @@
+package com.rufino.climedapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimedApiApplication.class, args);
+	}
+
+}
