2f5531e7b976a22bae5c53a9681cdd32ab64da84
jorge-rufino <jorge.rufino1986@gmail.com>
2023-12-18 15:39:40 -0300

    feat: Validando dados antes de salvar Medico

--- src/main/java/com/rufino/climedapi/controller/MedicoController.java
+++ src/main/java/com/rufino/climedapi/controller/MedicoController.java
@@ -5,6 +5,7 @@ import com.rufino.climedapi.dto.MedicoReduzidoDTO;
 import com.rufino.climedapi.model.Endereco;
 import com.rufino.climedapi.model.Medico;
 import com.rufino.climedapi.service.MedicoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,7 +22,7 @@ public class MedicoController {
     private MedicoService medicoService;
 
     @PostMapping
-    public ResponseEntity<Medico> cadastrar(@RequestBody MedicoDTO dto){
+    public ResponseEntity<Medico> cadastrar(@RequestBody @Valid MedicoDTO dto){
         return ResponseEntity.status(HttpStatus.CREATED).body(medicoService.salvar(dto));
     }
 
--- src/main/java/com/rufino/climedapi/dto/EnderecoDTO.java
+++ src/main/java/com/rufino/climedapi/dto/EnderecoDTO.java
@@ -1,12 +1,28 @@
 package com.rufino.climedapi.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public record EnderecoDTO(
+        @NotBlank
         String logradouro,
+
+        @NotBlank
         String bairro,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{5}-\\d{3}")
         String cep,
+
+        @NotBlank
         String cidade,
+
+        @NotBlank
         String uf,
+
         String complemento,
+
         String numero
 ) {
 }
