c3505e448b1eacd863646e84835df4455828f15b
jorge-rufino <jorge.rufino1986@gmail.com>
2023-12-21 13:57:16 -0300

    refactor: Corrigindo os imports

--- src/main/java/com/rufino/climedapi/controller/MedicoController.java
+++ src/main/java/com/rufino/climedapi/controller/MedicoController.java
@@ -2,19 +2,16 @@ package com.rufino.climedapi.controller;
 
 import com.rufino.climedapi.dto.MedicoDTO;
 import com.rufino.climedapi.dto.MedicoReduzidoDTO;
-import com.rufino.climedapi.mapper.MedicoMapper;
 import com.rufino.climedapi.model.Medico;
 import com.rufino.climedapi.service.MedicoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -29,7 +26,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<MedicoReduzidoDTO> listar(Pageable pageable){
+    public Page<MedicoReduzidoDTO> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable pageable){
         return medicoService.listar(pageable);
     }
 }
--- src/main/java/com/rufino/climedapi/service/MedicoService.java
+++ src/main/java/com/rufino/climedapi/service/MedicoService.java
@@ -7,12 +7,9 @@ import com.rufino.climedapi.model.Medico;
 import com.rufino.climedapi.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 public class MedicoService {
