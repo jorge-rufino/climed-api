14dedd425fa0af20f57bb21b021baa9479c135cf
jorge-rufino <jorge.rufino1986@gmail.com>
2023-12-21 15:21:01 -0300

    feat: Add Cadastro de Pacientes

--- src/main/java/com/rufino/climedapi/controller/PacienteController.java
+++ src/main/java/com/rufino/climedapi/controller/PacienteController.java
@@ -3,7 +3,10 @@ package com.rufino.climedapi.controller;
 import com.rufino.climedapi.dto.PacienteDTO;
 import com.rufino.climedapi.model.Paciente;
 import com.rufino.climedapi.service.PacienteService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +20,8 @@ public class PacienteController {
     private PacienteService pacienteService;
 
     @PostMapping
-    public Paciente cadastrar(@RequestBody PacienteDTO pacienteDTO){
+    public ResponseEntity<Paciente> cadastrar(@RequestBody @Valid PacienteDTO pacienteDTO){
 
-        return pacienteService.salvar(pacienteDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(pacienteService.salvar(pacienteDTO));
     }
 }
--- src/main/java/com/rufino/climedapi/service/PacienteService.java
+++ src/main/java/com/rufino/climedapi/service/PacienteService.java
@@ -3,6 +3,7 @@ package com.rufino.climedapi.service;
 import com.rufino.climedapi.dto.PacienteDTO;
 import com.rufino.climedapi.mapper.PacienteMapper;
 import com.rufino.climedapi.model.Paciente;
+import com.rufino.climedapi.repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +13,10 @@ public class PacienteService {
     @Autowired
     private PacienteMapper pacienteMapper;
 
+    @Autowired
+    private PacienteRepository repository;
+
     public Paciente salvar(PacienteDTO pacienteDTO){
-        return pacienteMapper.toPacienteEntity(pacienteDTO);
+        return repository.save(pacienteMapper.toPacienteEntity(pacienteDTO));
     }
 }
